4cfa9d1da00e6a463fa343ace04927b22aaf3e31
4539453 <4539453@gmail.com>
2022-10-02 05:59:23 +0300

    Move nginx config

--- SimpleWSGI/run.sh
+++ SimpleWSGI/run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
-set -eu -o pipefail
+# set -eu -o pipefail
 
 if [ "$(ps aux | grep '[n]ginx')" != "" ]; then
 	sudo kill $(ps aux | grep '[n]ginx' | awk '{print $2}')
 fi
-sudo /usr/sbin/nginx -c "$(pwd)/nginx.conf"
+
+sudo /usr/sbin/nginx -c "$(pwd)/nginx/nginx.conf"
+
 gunicorn --bind 192.168.122.108:8000 --workers=2 app:AppClass
